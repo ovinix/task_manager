cff46da71aba80c3bf439c93969204f1c593ad22
ovinix <dev.shvvolodymyr@hotmail.com>
2016-03-25 18:04:15 +0200

    Style Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :projects
+
+  validates :name, presence: true, length: { maximum: 50 }
 end
