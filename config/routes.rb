b12d65fdea3a2b4e42a24f2a43ee3ec58699e331
ovinix <dev.shvvolodymyr@hotmail.com>
2016-03-26 13:59:17 +0200

    Styling and Devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for  :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for  :users, :path => '',
+                      :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root        'users#index'
   # get 'users/index'
   # get 'users/show'
