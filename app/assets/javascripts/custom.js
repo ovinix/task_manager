86216dd0bf557e047544e24f9df4004e89826745
ovinix <dev.shvvolodymyr@hotmail.com>
2016-03-29 00:09:18 +0300

    Fix collapsed panel style bug

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,6 +1,14 @@
-/* bootstrap modal */
+/* bootstrap */
 $(document).ready(function() {
 	$(".modal").on('shown.bs.modal', function() {
 	    $(this).find("[autofocus]:first").focus();
+	});
+
+	$('.panel').on('hide.bs.collapse', function () {
+		$(this).css("border-radius", "4px");
+	});
+
+	$('.panel').on('show.bs.collapse', function () {
+		$(this).css("border-radius", "");
 	});
 });
